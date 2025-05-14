48dc7f8e7395993cd410568177fcfa2e07305f4b
Yagyik88 <raghuwanshiyagyik@gmail.com>
2025-05-14 23:53:22 +0530

    Update Class.java

--- src/main/java/com/app/Class.java
+++ src/main/java/com/app/Class.java
@@ -5,9 +5,67 @@ import com.Dao.CustomerDao;
 import com.Dao.EngineDao;
 
 public class Class {
-   public static void main(String[] args) {
-//	   CustomerDao.addAllDetails();
-//	   CarDao.viewAllCars();
-	   CustomerDao.viewAllCustomers();
-}
+  Scanner scan = new Scanner(System.in);
+	        int choice;
+
+	        do {
+	            System.out.println("======= Car Rental System =======");
+	            System.out.println("1. Add Customer");
+	            System.out.println("2. View All Customers");
+	            System.out.println("3. Allocate Car to Customer");
+	            System.out.println("4. Deallocate Car from Customer");
+	            System.out.println("5. Add Car");
+	            System.out.println("6. View All Cars");
+	            System.out.println("7. Add Engine");
+	            System.out.println("8. Add Customer + Car + Engine (All Details)");
+	            System.out.println("9. Exit");
+	            System.out.print("Enter your choice: ");
+
+	            choice = scan.nextInt();
+
+	            switch (choice) {
+	                case 1:
+	                    CustomerDao.takeInputs();
+	                    break;
+
+	                case 2:
+	                    CustomerDao.viewAllCustomers();
+	                    break;
+
+	                case 3:
+	                    CustomerDao.allocateCar();
+	                    break;
+
+	                case 4:
+	                    CustomerDao.deallocateCar();
+	                    break;
+
+	                case 5:
+	                    CarDao.addCarInputs();
+	                    break;
+
+	                case 6:
+	                    CarDao.viewAllCars();
+	                    break;
+
+	                case 7:
+	                    EngineDao.takeIp();
+	                    break;
+
+	                case 8:
+	                    CustomerDao.addAllDetails();
+	                    break;
+
+	                case 9:
+	                    System.out.println("Exiting application. Goodbye!");
+	                    break;
+
+	                default:
+	                    System.out.println("Invalid choice. Please try again.");
+	            }
+
+	        } while (choice != 9);
+
+	        scan.close();
+	    }
 }
